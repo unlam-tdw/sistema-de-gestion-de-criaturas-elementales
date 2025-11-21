5d73e4166344ee9e0020d171c7a16176b5c0a6e4
Valentin Virzi <v.virzi@hotmail.com>
2025-11-21 19:17:52 -0300

    feat: implement Phase 5

--- src/ar/edu/unlam/pbii/criaturas/CriaturaAncestral.java
+++ src/ar/edu/unlam/pbii/criaturas/CriaturaAncestral.java
@@ -9,7 +9,7 @@ public class CriaturaAncestral extends Criatura {
         if (nivelMaestria > 40) {
             this.estaInestable = true;
         }
-        
+
         int energiaObtenida = nivelMaestria * 3;
         int nuevoNivelEnergia = this.nivelEnergia + energiaObtenida;
         if (nuevoNivelEnergia > 200) {
@@ -28,4 +28,27 @@ public class CriaturaAncestral extends Criatura {
     public boolean esAncestral() {
         return true;
     }
+
+    @Override
+    public void interactuar(Criatura otra) {
+        boolean thisAncestral = this.esAncestral();
+        boolean otraAncestral = otra.esAncestral();
+
+        if (thisAncestral && otraAncestral) {
+            this.setNivelEnergia(this.getNivelEnergia() + 20);
+            otra.setNivelEnergia(otra.getNivelEnergia() + 20);
+            return;
+        }
+
+        if (thisAncestral || otraAncestral) {
+            Criatura ganador = thisAncestral ? this : otra;
+            Criatura perdedor = thisAncestral ? otra : this;
+
+            ganador.setNivelEnergia(ganador.getNivelEnergia() + 20);
+
+            int energiaRestante = perdedor.getEnergiaTotal() - 15;
+            perdedor.setNivelEnergia(Math.max(0, energiaRestante));
+        }
+        ;
+    };
 }
