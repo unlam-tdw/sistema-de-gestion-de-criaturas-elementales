b91d561e2d81e98fc1607a9e7104410e4d630bf1
Mateo Maccarone <maccaronemateo.dev@gmail.com>
2025-11-20 22:02:17 -0300

    feat(criaturas): add abstract class and enum

---
+++ src/ar/edu/unlam/pbii/criaturas/Criatura.java
@@ -0,0 +1,54 @@
+package ar.edu.unlam.pbii.criaturas;
+
+public abstract class Criatura {
+	protected String nombre;
+	protected int nivelEnergia;
+	protected AfinidadElemental afinidad;
+	protected boolean estaInestable;
+	
+	public Criatura(String nombre, int nivelEnergia, AfinidadElemental afinidad) {
+		this.nombre = nombre;
+		this.nivelEnergia = nivelEnergia < 0 ? 0 : nivelEnergia;
+		this.afinidad = afinidad;
+		this.estaInestable = false;
+	}
+	
+	public abstract void entrenar(int nivelMaestria);
+	
+	public abstract void pacificar();
+	
+	public int getEnergiaTotal() {
+		return nivelEnergia;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getNivelEnergia() {
+		return nivelEnergia;
+	}
+	
+	protected void setNivelEnergia(int nivelEnergia) {
+		this.nivelEnergia = nivelEnergia;
+	}
+	
+	public AfinidadElemental getAfinidad() {
+		return afinidad;
+	}
+	
+	public boolean estaInestable() {
+		return estaInestable;
+	}
+	
+	protected void setEstaInestable(boolean estaInestable) {
+		this.estaInestable = estaInestable;
+	}
+	
+	public boolean esAncestral() {
+		return false;
+	}
+	
+	public void interactuar(Criatura otra) {
+	}
+}
